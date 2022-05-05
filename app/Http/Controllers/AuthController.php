41198e55d59b8ca22fd257ce7387d2ff10a83951
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-05-05 20:05:04 +0300

    Create sanctum auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a User
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(
+            array_merge(
+                $request->validated(),
+                ['password' => Hash::make($request->password)]
+            )
+        );
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Log user in via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            return response(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function user()
+    {
+        return new UserResource(Auth::user());
+    }
+
+    /**
+     * Log the user out
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Auth::logout();
+
+        return response(['message' => 'Success'], Response::HTTP_OK);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer'
+        ]);
+    }
+}

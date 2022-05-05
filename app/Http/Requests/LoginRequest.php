3a4d76019fd952a8789501a902ca1ba62911c567
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-05-05 17:25:29 +0300

    Create validation rules

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required',
+            'email.email' => 'Incorrect Login field',
+            'password.required' => 'Password is required'
+        ];
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|between:2,100',
+            'surname' => 'required|string|between:2,50',
+            'email' => 'required|string|email|max:100|unique:users',
+            'phone' => 'required|regex:/(+27)[0-9]{8}/',
+            'password' => 'required|string|min:6',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'surname.required' => 'Surname is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Incorrect Email field',
+            'phone.required' => 'Phone is required',
+            'password.required' => 'Password is required'
+        ];
+    }
+}

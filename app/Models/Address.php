089736c2951ad2162ddc5685166099c6df307a79
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-05-13 12:11:39 +0300

    Create addresses data

---
+++ app/Models/Address.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($address) {
+            if (auth()->check()) {
+                $address->user_id = auth()->user()->id;
+            }
+
+            return response()->json('Unauthorized', 403);
+        });
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

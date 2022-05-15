c53d20c5803105eca8a508b28eab55a0b444d6ce
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-05-16 00:49:18 +0300

    Fix routes to api resource

--- routes/Mobile/api.php
+++ routes/Mobile/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Mobile\AuthController;
 use App\Http\Controllers\Mobile\UserController;
-use App\Http\Controllers\Mobile\OrderController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -12,11 +11,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('logout', [AuthController::class, 'logout']);
     Route::get('profile', [UserController::class, 'show']);
     Route::put('profile', [UserController::class, 'update']);
-    Route::get('order/{id}', [OrderController::class, 'show']);
-    Route::post('orders', [OrderController::class, 'store']);
     Route::apiResource('books', BookController::class);
     Route::apiResource('photos', PhotoController::class);
     Route::apiResource('covers', CoverController::class);
     Route::apiResource('addresses', AddressController::class);
     Route::apiResource('payment-methods', PaymentMethodController::class);
+    Route::apiResource('orders', OrderController::class)->only(['show', 'store']);
 });

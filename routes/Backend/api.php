5ca2cf0a74cfaed8adcc3e89450f678cb8bc924a
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-05-15 13:51:50 +0300

    Add orders routes

--- routes/Backend/api.php
+++ routes/Backend/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\BookController;
+use App\Http\Controllers\Backend\OrderController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:admin')->group(function () {
         Route::apiResource('users', UserController::class);
         Route::get('books', [BookController::class, 'index']);
+        Route::get('orders', [OrderController::class, 'index']);
+        Route::put('order/{id}', [OrderController::class, 'update']);
     });
 });
--- routes/Mobile/api.php
+++ routes/Mobile/api.php
@@ -3,14 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Mobile\AuthController;
 use App\Http\Controllers\Mobile\UserController;
+use App\Http\Controllers\Mobile\OrderController;
 
-Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('logout', [AuthController::class, 'logout']);
     Route::get('profile', [UserController::class, 'show']);
     Route::put('profile', [UserController::class, 'update']);
+    Route::get('order/{id}', [OrderController::class, 'show']);
+    Route::post('orders', [OrderController::class, 'store']);
     Route::apiResource('books', BookController::class);
     Route::apiResource('photos', PhotoController::class);
     Route::apiResource('covers', CoverController::class);
